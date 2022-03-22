4df57fefffc2d9800cf143fb03c6044c24934791
yusahoraa <felix.ollivier-drolshagen@next-u.fr>
2022-03-22 16:01:19 +0100

    First commit, basic mouvement and .gitignore

---
+++ Assets/Script/PlayerBehaviour.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _playerTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            _playerTransform.position += new Vector3(0.1f,0,0);
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            _playerTransform.position += new Vector3(-0.1f, 0, 0);
+        }
+
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            _playerTransform.position += new Vector3(0, 0.1f, 0);
+        }
+
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            _playerTransform.position += new Vector3(0, -0.1f, 0);
+        }
+
+    }
+}
